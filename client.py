442ed0df9bffc33818e7dec93b75b6d7ebb5a6ae
Ragul1902 <ragulsenthilkumar02@gmail.com>
2021-11-17 00:54:07 +0530

    Feature Update: messages can be sent in multiple colors

--- client.py
+++ client.py
@@ -1,28 +1,73 @@
 import socket
 import threading
+from colorama import Fore, Back, Style
+
 
 serverAddress = socket.gethostbyname(socket.gethostname())
 serverPort = 1234
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverAddress, serverPort))
 
+
+def printMessage(message):
+    
+    msgList = message.split()
+    if(len(msgList) >= 3 and msgList[1] == "/color"):
+
+        if(msgList[2] == "red"):
+            print(Fore.RED,end="")
+
+        elif(msgList[2] == "green"):
+            print(Fore.GREEN,end="")
+
+        elif(msgList[2] == "yellow"):
+            print(Fore.YELLOW,end="")
+
+        elif(msgList[2] == "blue"):
+            print(Fore.BLUE,end="")
+
+        elif(msgList[2] == "megenta"):
+            print(Fore.MAGENTA,end="")
+
+        elif(msgList[2] == "cyan"):
+            print(Fore.CYAN,end="")
+
+        elif(msgList[2] == "white"):
+            print(Fore.WHITE,end="")
+
+        else:
+            print(message)
+            return
+
+        message = msgList[0] + ' '.join(msgList[3:])
+        print(message)
+        print(Style.RESET_ALL,end="")
+    else:
+        print(message)
+        return
+
+
 def receiveMessage():
     while True:
         try:
             msgFromServer = clientSocket.recv(1024).decode()
-            print(msgFromServer)
-        except:
+            # print(msgFromServer)
+            printMessage(msgFromServer)
+        except KeyboardInterrupt:
             clientSocket.close()
-            break
+            return
+
+
 
 def sendMessage():
     while True:
         try:
             msgForServer = input()
             clientSocket.send(msgForServer.encode())
-        except:
+        except KeyboardInterrupt:
             clientSocket.close()
-            break
+            return
+
 
 receiveMessageThread = threading.Thread(target=receiveMessage)
 receiveMessageThread.start()
